e5a3dd5c313df3bc70d28ed5eec9d68329f98fc7
huangyong <756655135@qq.com>
2018-11-30 15:06:12 +0800

    HenCoder Practise2

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/PageFragment.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/PageFragment.java
@@ -2,6 +2,7 @@ package com.hencoder.hencoderpracticedraw2;
 
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -24,14 +25,14 @@ public class PageFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_page, container, false);
 
-        ViewStub sampleStub = (ViewStub) view.findViewById(R.id.sampleStub);
+        ViewStub sampleStub = view.findViewById(R.id.sampleStub);
         sampleStub.setLayoutResource(sampleLayoutRes);
         sampleStub.inflate();
 
-        ViewStub practiceStub = (ViewStub) view.findViewById(R.id.practiceStub);
+        ViewStub practiceStub = view.findViewById(R.id.practiceStub);
         practiceStub.setLayoutResource(practiceLayoutRes);
         practiceStub.inflate();
 
--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice14MaskFilterView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice14MaskFilterView.java
@@ -3,6 +3,7 @@ package com.hencoder.hencoderpracticedraw2.practice;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
@@ -15,6 +16,11 @@ import com.hencoder.hencoderpracticedraw2.R;
 public class Practice14MaskFilterView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     Bitmap bitmap;
+
+    BlurMaskFilter maskFilter1;
+    BlurMaskFilter maskFilter2;
+    BlurMaskFilter maskFilter3;
+    BlurMaskFilter maskFilter4;
 
     public Practice14MaskFilterView(Context context) {
         super(context);
@@ -31,6 +37,11 @@ public class Practice14MaskFilterView extends View {
     {
         setLayerType(LAYER_TYPE_SOFTWARE, null);
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.what_the_fuck);
+
+        maskFilter1 = new BlurMaskFilter(100, BlurMaskFilter.Blur.NORMAL);
+        maskFilter2 = new BlurMaskFilter(100, BlurMaskFilter.Blur.INNER);
+        maskFilter3 = new BlurMaskFilter(100, BlurMaskFilter.Blur.OUTER);
+        maskFilter4 = new BlurMaskFilter(100, BlurMaskFilter.Blur.SOLID);
     }
 
     @Override
@@ -40,15 +51,19 @@ public class Practice14MaskFilterView extends View {
         // 用 Paint.setMaskFilter 来设置不同的 BlurMaskFilter
 
         // 第一个：NORMAL
+        paint.setMaskFilter(maskFilter1);
         canvas.drawBitmap(bitmap, 100, 50, paint);
 
         // 第二个：INNER
+        paint.setMaskFilter(maskFilter2);
         canvas.drawBitmap(bitmap, bitmap.getWidth() + 200, 50, paint);
 
         // 第三个：OUTER
+        paint.setMaskFilter(maskFilter3);
         canvas.drawBitmap(bitmap, 100, bitmap.getHeight() + 100, paint);
 
         // 第四个：SOLID
+        paint.setMaskFilter(maskFilter4);
         canvas.drawBitmap(bitmap, bitmap.getWidth() + 200, bitmap.getHeight() + 100, paint);
     }
 }
